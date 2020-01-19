fbc29e83de507abc7dd858411f216fd57bd0f4dd
strawberry0325 <56204933+strawberry0325@users.noreply.github.com>
2020-01-20 03:12:58 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,40 @@
 #include <stdio.h>
 
+void Swap(int*a,int*b);
+
 int main(int argc, char *argv[]) 
 {
     char str[7];
+    int data[100];
+    int i,j,k,count=0;
     
     while(fgets(str, sizeof(str), stdin)){
-        printf("%s", str);
+        data[count]=atoi(str);
+        count++;
+    }
+    
+    for(i=0;i<count;i++)
+        printf("%d ",data[i]);
+    putchar('\n');
+    
+    for(i=0;i<count;i++){
+        for(j=count-1;j>i;j--){
+            if(data[j]<data[j-1]){
+                Swap(&data[j],&data[j-1]);
+                for(k=0;k<count;k++)
+                    printf("%d ",data[k]);
+                putchar('\n');
+        }
     }
 
     return 0;
+}
+
+void Swap(int*a,int*b)
+{
+    int tmp;
+    
+    temp=*a;
+    *a=*b;
+    *b=tmp;
 }
